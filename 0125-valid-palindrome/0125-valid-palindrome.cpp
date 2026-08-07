eba66472ce24f88a52a2e8d6c7a8a2156033400f
Shivraj Nalawade <shivrajnalawade77@gmail.com>
2026-08-07 23:46:45 +0530

    Time: 0 ms (100%), Space: 9.8 MB (78.05%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAlphaNumeric(char ch){
+        if((ch>='0' && ch<='9') |
+         (tolower(ch)>='a' &&  tolower(ch)<='z')){
+            return true;
+         }
+
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int st = 0, end = s.length()-1;
+
+        while(st < end){
+            if(!isAlphaNumeric(s[st])){
+                st++;
+                continue;
+            }
+
+            if(!isAlphaNumeric(s[end])){
+                end--;
+                continue;
+            }
+            
+            if(tolower(s[st]) != tolower(s[end])){
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+};
