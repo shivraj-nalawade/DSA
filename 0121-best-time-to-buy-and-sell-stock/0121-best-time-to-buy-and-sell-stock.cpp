3beb67f3ab7649daf6937ce992bc38d8a294af5e
Shivraj Nalawade <shivrajnalawade77@gmail.com>
2026-08-18 22:35:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int bestBuy = prices[0];
+        int maxProfit = 0;
+        for(int i=1; i<n; i++){
+            if(prices[i] > bestBuy){
+                maxProfit = max(maxProfit, prices[i]- bestBuy);
+            }
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
